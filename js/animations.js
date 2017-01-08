e39b525e032ae37674cccf8dee9d99de0248e936
Rurikari <jeyakavin@hotmail.com>
2017-01-07 23:49:37 -0500

    Added whole bunch of stuff

---
+++ js/animations.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#Carousel').carousel({
+        interval: 5000
+    })
+});
+
+$(document).ready(function() {
+    $('#Carousel2').carousel({
+        interval: 5000
+    })
+});
+
+$(document).ready(function() {
+    $('#Carousel3').carousel({
+        interval: 5000
+    })
+});
+
+$(document).ready(function() {
+    $('#Carousel4').carousel({
+        interval: 5000
+    })
+});
